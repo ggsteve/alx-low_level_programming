b802c800f34ababbd7098587d9439f520f87fcbd
ggsteve <salilyassine@gmail.com>
2023-10-08 10:55:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main(void){
-
+/**
+ * desciption : Main function
+ * return :  End the program with the value 0
+ */ 
 puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
