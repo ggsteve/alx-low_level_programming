0eeea0acfa62bc840f11ae0d4ba405157c7b4250
ggsteve <salilyassine@gmail.com>
2023-10-08 11:40:53 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a piece of art with proper grammar.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("%s%s%s%s%s\n",
+	       "with proper grammar, but the outcome is a ",
+	       "piece of art,", " ",
+	       "followed by a new line.",
+	       "");
+
+	return (0);
+}
