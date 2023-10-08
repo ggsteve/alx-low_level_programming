4540760509c8154e1d25860c480a177e9958da4e
ggsteve <salilyassine@gmail.com>
2023-10-08 12:41:54 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,14 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program writes a specified text to the standard error.
+ *
+ * Return: Always 1 (error)
+ */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
