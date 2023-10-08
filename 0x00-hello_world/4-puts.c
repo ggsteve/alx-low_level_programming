3b6d0816c61429e7bbbeb3b7124a29ec2c35186c
ggsteve <salilyassine@gmail.com>
2023-10-08 10:30:46 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-int main(void) {
 
-puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+    // Use the puts function to print the string
+    puts("Programming is like building a multilingual puzzle");
 
-return 0;
-
+    // End the program with the value 0
+    return 0;
 }
