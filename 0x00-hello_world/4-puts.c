394d76fbe566a318b8b446707ae91e947b0dfb7b
ggsteve <salilyassine@gmail.com>
2023-10-08 10:46:48 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
+int main(void){
 
-int main(void) {
-    // Use the puts function to print the string
 puts("\"Programming is like building a multilingual puzzle");
 
-    // End the program with the value 0
 return (0);
 }
