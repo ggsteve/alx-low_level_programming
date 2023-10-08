53e0ce11d77143373a05d22ae57a55c83784a8d3
ggsteve <salilyassine@gmail.com>
2023-10-08 11:56:48 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,10 +9,10 @@
  */
 int main(void)
 {
-    printf("%s%s%s\n",
-           "with proper grammar, but the outcome is a ",
-           "piece of art,",
-           "");
+	printf("%s%s%s\n",
+	       "with proper grammar, but the outcome is a ",
+	       "piece of art,",
+	       "");
 
-    return (0);
+	return (0);
 }
