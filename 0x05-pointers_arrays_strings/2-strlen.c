91f3879ebe3f75dafa6067c0b68b1d287280c6bf
ggsteve <salilyassine@gmail.com>
2023-10-16 14:37:12 -0700

    poi

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,12 +7,11 @@
  */
 size_t _strlen(const char *str)
 {
-    size_t length = 0;
+	size_t length = 0;
 
-    while (*str++)
-        length++;
-    
+	while (*str++)
+		length++;
 
-    return length;
+	return (length);
 }
 	
