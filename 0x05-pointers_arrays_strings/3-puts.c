10ffeaabd637cb903c5e6accaebd3afc784e8a82
ggsteve <salilyassine@gmail.com>
2023-10-16 15:26:42 -0700

    tt

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,12 +7,10 @@
  * Return: void.
  */
 void _puts(char *str)
-
 {
-    for (; *str != '\0'; str++)
-    {
-        _putchar(*str);
-    }
-    _putchar('\n');
-
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
 }	 
