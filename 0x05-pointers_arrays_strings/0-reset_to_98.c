57be4098fe256b6b54fb83715163eec6f50962a7
ggsteve <salilyassine@gmail.com>
2023-10-16 12:14:04 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
 
-/**
- * reset_to_98 - Updates the value pointed to by a pointer to 98.
- * @n: Pointer to an integer.
- */
-void reset_to_98(int *n)
-{
-	/* Dereference the pointer and set the value to 98 */
-	*n = 98;
-}
+/* Function prototype */
+void reset_to_98(int *n);
 
-/**
- * main - Entry point of the program.
- * Return: Always 0.
- */
 int main(void)
 {
-	int number = 42;
+    int number = 42;
 
-	/* Display the original value */
-	printf("Original value: %d\n", number);
+    /* Display the original value */
+    printf("Original value: %d\n", number);
 
-	/* Call the function to reset the value to 98 */
-	reset_to_98(&number);
+    /* Call the function to reset the value to 98 */
+    reset_to_98(&number);
 
-	/* Display the updated value */
-	printf("Updated value: %d\n", number);
+    /* Display the updated value */
+    printf("Updated value: %d\n", number);
 
-	return (0);
+    return 0;
 }
 
