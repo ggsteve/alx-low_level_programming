60d1886959d92df27d32339c50cb80fa43cb829a
ggsteve <salilyassine@gmail.com>
2023-10-16 16:10:48 -0700

    y

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print rev - Prints a string in reverse
+ * @Gs: The string to print
+ * Retin: void
+ */
+
+void print_rev(char *s)
+    
+{
+	int c = 0;
+      while (s[c] != '\0') 
+      {
+      c++;
+      }      
+      
+      for (c -= 1; c >= 0; c--)
+      {	      
+      putchar(s[c]);
+      }
+      _putchar ('\n');
+}
