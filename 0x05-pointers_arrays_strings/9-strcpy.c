bdedb74408011f123c0b350ace04565a12877fc4
ggsteve <salilyassine@gmail.com>
2023-10-17 13:06:39 -0700

    x

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * _strcpy - copy a string 
+ * @dest: destination value
+ * @src: source value
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char  *src)
+{
+/* Declare variables */
+int i;
+
+for (i = 0; src[i] != '\0'; i++)
+{	
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);		
+}
