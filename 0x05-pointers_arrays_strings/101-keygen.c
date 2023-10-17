46a064554aeff85cd1e8cc61ca6cef47f6a5da42
ggsteve <salilyassine@gmail.com>
2023-10-17 14:36:32 -0700

    t

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,34 +3,46 @@
 #include <time.h>
 #include <string.h>
 
-// Function to generate a random password
+/* Function to generate a random password */
 void generatePassword(char *password, int length) {
-    // Define the characters allowed in the password
+    /* Define the characters allowed in the password */
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    // Seed the random number generator
+    /* Seed the random number generator */
     srand((unsigned int)time(NULL));
 
-    // Generate the password
-    for (int i = 0; i < length; ++i) {
+    /* Generate the password */
+    int i;
+    for (i = 0; i < length; ++i) {
         int index = rand() % (int)(sizeof(charset) - 1);
         password[i] = charset[index];
     }
 
-    // Null-terminate the password
+    /* Null-terminate the password */
     password[length] = '\0';
 }
 
 int main() {
-    // Assuming the maximum password length is 10 characters
+    /* Assuming the maximum password length is 10 characters */
     const int passwordLength = 10;
-    char password[passwordLength + 1];
+    
+    /* Use dynamic memory allocation instead of variable-length array */
+    char *password = (char *)malloc((passwordLength + 1) * sizeof(char));
 
-    // Generate a random password
+    /* Check if memory allocation is successful */
+    if (password == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+
+    /* Generate a random password */
     generatePassword(password, passwordLength);
 
-    // Print the generated password
+    /* Print the generated password */
     printf("Generated Password: %s\n", password);
+
+    /* Free the dynamically allocated memory */
+    free(password);
 
     return 0;
 }
